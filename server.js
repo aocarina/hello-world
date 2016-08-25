a6cf2aff80c64fc3fbb32d37f5bcd8ddac36fa2a
Alan Sikora <alansikora@gmail.com>
2016-08-25 18:49:49 -0300

    initial

---
+++ server.js
@@ -0,0 +1,36 @@
+var http = require('http')
+var MongoClient = require('mongodb').MongoClient
+var connected = false
+var db
+
+var respond = function(res, what) {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end(what)
+}
+
+MongoClient.connect('mongodb://mongo_container:27017/docker', function(err, dbi) {
+  if(err) connected = err
+
+  connected = true
+
+  db = dbi
+})
+
+var app = http.createServer(function(req, res) {
+  if (connected !== true) {
+    return respond(res, 'Errrrrou:' + connected)
+  }
+
+  db.collection('counter').insert({ hey: true }, function(err, result) {
+    if (err) respond(res, err)
+
+    db.collection('counter').find().count(function(err, count) {
+      if (err) respond(res, err)
+
+      respond(res, 'Vocêêêê acessou ' + count + ' vezeeeeeees')
+    })
+  })
+}).listen(1337, "0.0.0.0")
+
+//sets port and IP address of the server
+console.log('Server running at http://0.0.0.0:1337/')
